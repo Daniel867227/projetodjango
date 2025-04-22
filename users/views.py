a54c2d2d4994406a8bcd7d6732858f86528208de
Daniel <daniel.mdsc91@gmail.com>
2025-04-22 00:31:11 -0300

    continuacao do projeto

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth import logout, authenticate
+from django.contrib.auth import login as login_auth
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+
 
 
 
@@ -11,15 +13,19 @@ def logout_view(request):
     return HttpResponseRedirect(reverse('index'))
 
 def register(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('index'))
+    
     if request.method != 'POST':
         form = UserCreationForm()
     else:
         form = UserCreationForm(data =request.POST)
         if form.is_valid():
             new_user = form.save()
-            authenticate_user = authenticate(username=new_user.username, password=request.POST['password1'])
-            login(request, authenticate_user)
-            return HttpResponseRedirect(reverse('index'))
+            #authenticate_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            #login(request, authenticate_user)
+            #return HttpResponseRedirect(reverse('index'))
 
     context = {'form': form }
     return render(request, 'users/register.html', context)
+
